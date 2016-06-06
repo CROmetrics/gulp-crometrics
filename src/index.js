1b0d50aee5a3e63a873a0b13e5de40baa2ca9b20
eclecto <newlandericj@gmail.com>
2016-06-06 14:42:27 -0400

    Fixes to global logic

--- src/index.js
+++ src/index.js
@@ -12,25 +12,33 @@ import finder from 'process-finder';
 
 const paths = {
   src: {
-    html: '/src/**/*.html',
-    scripts: '/src/**/*.js',
-    stylesheets: ['/src/**/*.css', '/src/**/*.scss', '/src/**/*.sass'],
+    html: 'src/**/*.html',
+    scripts: 'src/**/*.js',
+    stylesheets: ['src/**/*.css', 'src/**/*.scss', 'src/**/*.sass'],
+    all: 'src/**/*.+(html|js|css|scss|sass)'
   },
-  html: '/src/*.html',
-  scripts: '/src/*.js',
-  stylesheets: ['/src/*.css', '/src/*.scss', '/src/*.sass'],
+  html: 'src/*.html',
+  scripts: 'src/*.js',
+  stylesheets: ['src/*.css', 'src/*.scss', 'src/*.sass'],
   dest: '',
 };
+
+const sassOptions = {
+  outputStyle: 'compressed',
+  sourceMapEmbed: false
+}
 
 export default function clearbuild(_gulp, basePath, { lintCss = false } = {}) {
   const gulp = gulpHelp(_gulp);
   const sequence = gulpSeq.use(gulp);
   
+  var globalPath = basePath.split('/')[0];
+  
   // Executable Dev function to compile and watch for further file changes
   this.dev = () => {
     return sequence(
       ['lint:scripts', 'lint:stylesheets'],
-      'build:stylesheets', 'build:scripts'
+      'build:stylesheets', 'build:scripts',
       'watch'
     )();
   };
@@ -49,51 +57,48 @@ export default function clearbuild(_gulp, basePath, { lintCss = false } = {}) {
   gulp.task('dev', 'Build and preview your experiment.', this.dev);
 
   gulp.task('watch', 'Rebuild when experiment files change.', () => {
-    gulp.watch(basePath + paths.src.html, ['build']);
-    gulp.watch(basePath + paths.src.scripts, ['build']);
-    gulp.watch(basePath + paths.src.stylesheets, ['build']);
+    gulp.watch(basePath + '/' + paths.src.all, ['build']);
     
     // Watch global
-    gulp.watch(paths.src.html, ['build']);
-    gulp.watch(paths.src.scripts, ['build']);
-    gulp.watch(paths.src.stylesheets, ['build']);
+    gulp.watch(globalPath + '/' + paths.src.all, ['build']);
   });
 
   // -- Build Experiment ----------
   gulp.task('build', 'Build experiment scripts and stylesheets.', () => {
+    console.log('Change detected. Compiling...');
     return sequence('lint', 'build:clean', 'build:stylesheets', 'build:scripts')();
   });
 
   gulp.task('build:clean', 'Clean build.', (cb) => {
-    return del([basePath + paths.dest + '/*.js', basePath + paths.dest + '/*.css'], cb);
+    return del([basePath + '/*.js', basePath + '/*.css', globalPath + '/*.js', globalPath + '/*.css'], cb);
   });
 
   gulp.task('build:scripts', 'Transpile and browserify scripts.', ['build:scripts:global','build:scripts:variation']);
   
   gulp.task('build:scripts:global', () => {
-    return gulp.src(paths.scripts)
+    return gulp.src(globalPath + '/' + paths.scripts)
       .pipe(bundlify())
-      .pipe(gulp.dest(paths.dest));
+      .pipe(gulp.dest(globalPath));
   });
   
   gulp.task('build:scripts:variation', () => {
-    return gulp.src(basePath + paths.scripts)
+    return gulp.src(basePath + '/' + paths.scripts)
       .pipe(bundlify())
-      .pipe(gulp.dest(basePath + paths.dest));
+      .pipe(gulp.dest(basePath));
   });
   
   gulp.task('build:stylesheets', 'Compile stylesheets.', ['build:stylesheets:global','build:stylesheets:variation']);
 
   gulp.task('build:stylesheets:global', 'Compile stylesheets.', () => {
-    return gulp.src(paths.stylesheets)
-      .pipe(gulpSass({ sourceMapEmbed: false }).on('error', gulpSass.logError))
-      .pipe(gulp.dest(paths.dest));
+    return gulp.src(globalPath + '/src/global.scss')
+      .pipe(gulpSass(sassOptions).on('error', gulpSass.logError))
+      .pipe(gulp.dest(globalPath));
   });
 
   gulp.task('build:stylesheets:variation', 'Compile stylesheets.', () => {
-    return gulp.src(basePath + paths.stylesheets)
-      .pipe(gulpSass({ sourceMapEmbed: false }).on('error', gulpSass.logError))
-      .pipe(gulp.dest(basePath + paths.dest));
+    return gulp.src(basePath + '/' + paths.stylesheets)
+      .pipe(gulpSass(sassOptions).on('error', gulpSass.logError))
+      .pipe(gulp.dest(basePath));
   });
 
   // -- Lint Experiment ----------
@@ -103,13 +108,13 @@ export default function clearbuild(_gulp, basePath, { lintCss = false } = {}) {
   );
 
   gulp.task('lint:scripts', 'Lint scripts.', () => {
-    return gulp.src(basePath + paths.scripts)
+    return gulp.src(basePath + '/' + paths.scripts)
       .pipe(eslint(eslintConfig))
       .pipe(eslint.format());
   });
 
   gulp.task('lint:stylesheets', 'Compile and lint stylesheets.', () => {
-    const task = gulp.src(basePath + paths.stylesheets)
+    const task = gulp.src(basePath + '/' + paths.stylesheets)
       .pipe(gulpSass({ sourceMapEmbed: false }).on('error', gulpSass.logError))
       .pipe(csslint(csslintConfig));
     if (lintCss) {
